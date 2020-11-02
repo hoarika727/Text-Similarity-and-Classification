509fc92b3be311c0703d47b24af7b3ee0f675aa0
Hsuan-An Weng Lin <lancewenglin@gmail.com>
2020-11-02 08:23:09 -0600

    Add Passive Aggressive with if-idf

---
+++ passive_aggressive.py
@@ -0,0 +1,49 @@
+# %%
+import numpy as np
+import pandas as pd
+import itertools
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+import nltk
+
+# %%
+df = pd.read_csv('data/train.csv')
+df.head(10)
+
+# %%
+nltk.download('words')
+words = set(nltk.corpus.words.words())
+
+def filter_words(text):
+    '''a function for removing the non English words'''
+    # removing the stop words and lowercasing the selected words
+    text = [word.lower() for word in text.split() if word.lower() not in words]
+    # joining the list of words with space separator
+    return " ".join(text)
+
+# %%
+# not helping to improve the accuracy
+# df['title1_en'] = df['title1_en'].apply(filter_words)
+# df['title2_en'] = df['title2_en'].apply(filter_words)
+
+# %%
+#DataFlair - Split the dataset
+x_train,x_test,y_train,y_test=train_test_split(df['title1_en']+df['title2_en'], df['label'], test_size=0.2, random_state=7)
+
+# %%
+#DataFlair - Initialize a TfidfVectorizer
+tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
+#DataFlair - Fit and transform train set, transform test set
+tfidf_train=tfidf_vectorizer.fit_transform(x_train) 
+tfidf_test=tfidf_vectorizer.transform(x_test)
+# %%
+#DataFlair - Initialize a PassiveAggressiveClassifier
+pac=PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train,y_train)
+#DataFlair - Predict on the test set and calculate accuracy
+y_pred=pac.predict(tfidf_test)
+score=accuracy_score(y_test,y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
+# %%
